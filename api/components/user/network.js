8e225fc07f8af4c915bf6e4007077a75498b8b02
ESanti <santiellenacm@gmail.com>
2021-06-11 12:11:01 -0300

    Api y mongoservice api conectados

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+const controller = require('./index');
+const response = require('../../../network/response');
+
 router.get('/', (req, res) => {
 
-    res.send('Server working');
+    controller.list()
+    .then(data => {
+        response.success(req, res, data, 200);
+    })
+    .catch(e => {
+        response.error(req, res, e, 400);
+    });
 });
 
 module.exports = router;
