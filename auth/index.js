0a2cbb8c777ad3eb2928b4de5654c97e508e6c8c
ESanti <santiellenacm@gmail.com>
2021-06-14 15:03:48 -0300

    Verificacion por JWT

--- auth/index.js
+++ auth/index.js
@@ -8,4 +8,50 @@ function auth(data){
     return token
 }
 
-module.exports = auth
+const verify = (token) => {
+    try{
+        return jwt.verify(token, secret);
+    }catch(error){
+        throw error(error, 401);
+    };
+}
+
+const check = {
+    own: (req, owner) => {
+        const decoded = decodeHeader(req);
+        if(decoded.id !== owner){
+
+            throw error('Access denied', 401);
+        };
+        return true;
+    },
+
+}
+
+const getToken = (auth) => {
+
+    if(!auth){
+        throw error('There is not TOKEN', 401);
+    }
+    if(auth.indexOf('Bearer ', '') == -1){
+        throw new Error('Incorrect TOKEN information', 401);
+    }
+
+    let token = auth.replace('Bearer ', '');
+    
+    return token;
+}
+
+const decodeHeader = (req) => {
+    const authorization = req.headers.authorization || '';
+    const token = getToken(authorization);
+    const decoded = verify(token);
+
+    req.user = decoded;
+    return decoded;
+};
+
+module.exports = {
+    auth,
+    check,
+}
